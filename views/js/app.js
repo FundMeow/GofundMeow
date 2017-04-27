d9194d68589ebcd24a1ee703f5199312fe9cd305
Joshua Brummet <JoshuaBrummet@Joshuas-MacBook-Pro-2.local>
2017-04-27 16:12:55 -0600

    updated app.js, pets.html, users.html

--- views/js/app.js
+++ views/js/app.js
@@ -6,44 +6,56 @@
 
 var app = angular.module('fundMeow',[]);
 
-app.controller('petCtrl', ['$scope', '$http', function($scope, $http) {
+
+app.factory('userService', function(){
+    var _user = {};
+    function set(data){
+        _user.push(data);
+        console.log(_user);
+    }
+    function get(){
+        console.log(user);
+        return _user
+    }
+    return {
+        set: set,
+        get: get
+    }
+});
+
+app.controller('petCtrl', ['$scope', '$http','userService', function($scope, $http, userService) {
 
     $http.get('/users').then(function(data){
         $scope.users = data;
         $scope.pets = [];
+        console.log($scope.users);
         for(var i = 0; i < $scope.users.data.users.length; i++){
             $scope.pets.push($scope.users.data.users[i].pet);
+            $scope.pets[i]._id = $scope.users.data.users[i]._id;
         }
         console.log($scope.pets);
     });
 
     $scope.profile = function(userId){
-        console.log(userId);
-        // $scope._id = userId;
-        // $http.get('/user'+ userId).then(function(data){
-        //     console.log(data);
-        // })
+        userService.set(userId);
+        $http.get('/user/' + userId).then(function(data){
+            console.log(data.data.user);
+            $scope._user = data.data.user;
+        })
     }
+}]);
+
+app.controller('userCtrl', ['$scope', '$http', 'userService', function($scope, $http, userService) {
+
+    var user_id = userService.get();
+    console.log(user_id);
+    $http.get('/user/' + user_id).then(function(data){
+        console.log(data);
+        $scope._user = data.data.user;
+    })
 
 }]);
-app.controller('userCtrl', ['$scope', '$http', function($scope, $http) {
 
-    // var user_id = $scope._id;
-    // console.log(user_id);
-    //
-    //
-    // $http.get('/user'+ user_id).then(function(data){
-    //     console.log(data);
-    //     // $scope.user = data;
-    //     // $scope.pet = [];
-    //     // for(var i = 0; i < $scope.user.data.pet.length; i++){
-    //     //     $scope.pets.push($scope.user.data.user[i].pet);
-    //     // }
-    //     // console.log($scope.pet);
-    //
-    // })
-
-}]);
 app.controller('imageCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.savePhoto = function () {
         var fd = new FormData();
